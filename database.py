373a8e2fbb9ae70804e7f898682d72bfb632128f
Sina <www.mahonia@gmail.com>
2026-09-05 15:09:08 +0330

    create table method was created and books table was imported

--- database.py
+++ database.py
@@ -1,11 +1,25 @@
 import sqlite3
 
 class Database:
-    def connector(self):
+    def connect(self):
         self.connection = sqlite3.connect("library.db")
 
-    def cursoring(self):
+    def create_cursor(self):
         self.cursor = self.connection.cursor()
 
     def create_table(self):
-        pass
+        self.cursor.execute("""
+    CREATE TABLE IF NOT EXISTS books (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        author TEXT,
+        genre TEXT,
+        publication_year INTEGER,
+        status TEXT
+    )
+""")
+
+
+db = Database()
+db.connect()
+db.create_cursor()
