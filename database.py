856576404bfe88cf11afa116190e8084054f8629
Mr.Sina Nosrati <sinatrader2004@gmail.com>
2026-09-05 15:49:15 +0330

    library database was created

--- database.py
+++ database.py
@@ -9,17 +9,20 @@ class Database:
 
     def create_table(self):
         self.cursor.execute("""
-    CREATE TABLE IF NOT EXISTS books (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        author TEXT,
-        genre TEXT,
-        publication_year INTEGER,
-        status TEXT
-    )
-""")
+            CREATE TABLE IF NOT EXISTS books (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            author TEXT,
+            genre TEXT,
+            publication_year INTEGER,
+            status TEXT
+                )
+        """)
+
+        self.connection.commit()
 
 
 db = Database()
 db.connect()
 db.create_cursor()
+db.create_table()
